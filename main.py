c650614de3862058367ac7fba7debd4989a32c6d
azort <c@zea.cl>
2023-08-17 18:26:46 -0400

    extraer_url_youtube

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import random
 import gradio as gr
+import re
+
+def extraer_url_youtube(mensaje):
+    # Expresión regular para detectar URLs de YouTube
+    youtube_regex = r'(https?://www\.youtube\.com/watch\?v=[\w-]+)'
+    match = re.search(youtube_regex, mensaje)
+    return match.group(0) if match else None
 
 def random_response(message, history):
-    return random.choice(["Yes", "No"])
+    extraer_url_youtube(message)
+    return extraer_url_youtube(message)
 
 demo = gr.ChatInterface(random_response)
 
